c29644d71ec97ba575de5d25b2489e62e3b8b42d
mgechev <mgechev@gmail.com>
2017-03-31 19:31:57 -0700

    feat: interactive prompt

---
+++ README.md
@@ -0,0 +1,25 @@
+# Automated Migration
+
+### Context-aware automated migration of `<template>` to `<ng-template>`.
+
+This tool will automatically migrate your templates from the deprecated `<template>` element to the new `<ng-template>` introduced by Angular 4.
+
+The replacement is context-aware, which means that it will consider the semantics of your code; this makes it better than simple "Search/Replace" with `sed` or other similar tools.
+
+Under the hood the tool parses your code with the Angular Compiler, [codelyzer](https://github.com/mgechev/codelyzer) and [tslint](https://github.com/palantir/tslint). Once the code is properly analyzed, it goes through a process of migration which replaces all occurrences of `<template>` with `<ng-template>`.
+
+# Warning
+
+**Do not perform the migration on code which is not under version control!**
+
+Although the tool is well tested, glitches are possible. In order to not loose information, perform the migration under version control and check the diff once done.
+
+# How to use it?
+
+```
+$ npm run migrate [PATH_TO_YOUR_FILES]
+```
+
+# License
+
+MIT
---
+++ migrate.js
@@ -0,0 +1,69 @@
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+const exec = require('child_process').execSync;
+const fs = require('fs');
+
+const newline = () => console.log();
+
+console.log(fs.readFileSync('./resources/banner').toString());
+
+newline();
+console.log(chalk.green('Let\'s migrate your templates!'));
+newline();
+
+const files = process.argv[2];
+
+if (!files) {
+  console.log(chalk.red('Path is not provided!'));
+  console.log(chalk.white('Please use the tool with:'));
+  newline();
+  console.log(chalk.gray('   npm run migrate [PATH_TO_YOUR_FILES]'));
+  newline();
+  process.exit(0);
+}
+
+const command = './node_modules/.bin/tslint -c migrate.json ' + files;
+let foundErrors = false;
+
+try {
+  console.log(exec(command).toString());
+} catch (e) {
+  foundErrors = true;
+
+  if (e && e.output && e.output[1]) {
+    console.log(chalk.cyan.bold('I found these errors:'));
+    console.log(e.output[1].toString());
+
+    inquirer.prompt([
+      {
+        type: 'confirm',
+        message: 'Do you want me to fix them for you?',
+        name: 'migrate'
+      }
+    ])
+    .then(function (res) {
+      if (res.migrate) {
+        newline();
+        console.log(exec(command + ' --fix').toString());
+        console.log(chalk.cyan.bold('Done!'));
+        newline();
+        console.log(chalk.yellow('⚠️  Do not forget to "diff" the changes!'));
+        newline();
+      } else {
+        newline();
+        console.log(chalk.cyan('No problems!'), chalk.cyan.bold('Better take care of those before the next major release!'));
+        newline();
+      }
+    }).catch(function (e) {
+      if (e && e.output && e.output[1]) {
+        console.log(chalk.cyan.bold('Uuupsss. I hit an error:'));
+        console.log(e.output[1].toString());
+      }
+    });
+  }
+}
+
+if (!foundErrors) {
+  console.log(chalk.green.bold('I didn\'t find anything wrong! All seems fine!'));
+  newline();
+}
